bab06821af7242a2b1b3810a8d71198997b359cf
sh-list <sharonirish@Sharons-iMac.local>
2017-11-14 14:41:42 +0000

    initial_ratings_layout
    • w/ basic responsiveness implemented

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,8 @@ var gulp = require('gulp'),
     // Browser-sync serve task
     gulp.task('browser-sync', function(){
         browsersync.init({
-            proxy: 'http://dev.www.list.co.uk/'
+            // proxy: 'http://dev.www.list.co.uk/'
+            proxy: 'http://dev.food.list.co.uk'
             // proxy: 'http://dev.edinburghfestival.list.co.uk/'
         });
     });
@@ -60,8 +61,8 @@ gulp.task('build/admin', function(){
         cssnano({discardUnused: false})
     ];
 
-    var lessStream = gulp.src('/Volumes/sites/less/awe-less/www/screen.less')
-    // var lessStream = gulp.src(['/Volumes/sites/less/awe-less/food/screen.less'])
+    // var lessStream = gulp.src('/Volumes/sites/less/awe-less/www/screen.less')
+    var lessStream = gulp.src(['/Volumes/sites/less/awe-less/food/screen.less'])
     // var lessStream = gulp.src(['/Volumes/sites/less/dev-less/film/screen.less'])
     // var lessStream = gulp.src(['/Volumes/sites/less/awe-less/edinburghfestival/screen.less'])
     // var lessStream = gulp.src(['/Volumes/sites/less/awe-less/update/update.less'])   // UPDATE form page
@@ -82,8 +83,8 @@ gulp.task('build/admin', function(){
 
     .pipe(postcss(processors))
 
-    .pipe(gulp.dest('/Volumes/sites/files.list.co.uk/assets/css/www')); // compile last to rectify skewing of styles caused by processing of other files
-    // .pipe(gulp.dest('/Volumes/sites/files.list.co.uk/assets/css/food'));
+    // .pipe(gulp.dest('/Volumes/sites/files.list.co.uk/assets/css/www')); // compile last to rectify skewing of styles caused by processing of other files
+    .pipe(gulp.dest('/Volumes/sites/files.list.co.uk/assets/css/food'));
     // .pipe(gulp.dest('/Volumes/sites/files.list.co.uk/assets/css/film'));
     // .pipe(gulp.dest('/Volumes/sites/files.list.co.uk/assets/css/edinburghfestival'));
     // .pipe(gulp.dest('/Volumes/sites/files.list.co.uk/assets/css/www'));    // UPDATE form page
